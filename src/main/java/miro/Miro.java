7f00df5d1e2ae43e7e6083817b7461cbf9c41f90
Nishadds <nishaddatascientist@gmail.com>
2022-09-24 14:28:58 +0300

    cleaned file

--- src/main/java/miro/Miro.java
+++ src/main/java/miro/Miro.java
@@ -41,16 +41,6 @@ public class Miro {
 
         // get the actual value of the title
         actualTitle = driver.getTitle();
-
-        /*
-         * compare the actual title of the page with the expected one and print
-         * the result as "Passed" or "Failed"
-         */
-        if (actualTitle.contentEquals(expectedTitle)){
-            System.out.println("Test Passed!");
-        } else {
-            System.out.println("Test Failed");
-        }
         Assert.assertEquals(actualTitle, expectedTitle);
     }
 
